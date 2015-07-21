d19a5c46a552d4a128cceb41320a1b6e7e0cc1eb
sekka <sekka3@gmail.com>
2015-07-22 01:36:05 +0900

    v0.0.3

--- setup.sh
+++ setup.sh
@@ -9,11 +9,26 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 # ==========================================================
 # 
-# homebrewを導入する
+# zshを導入する
 # 
 # ==========================================================
 
 brew install zsh
+
+# デフォルトのShellをzshにする
+# /etc/shells の末尾に /usr/local/bin/zsh を追記
+sudo sh -c 'echo $(which zsh) >> /etc/shells'
+
+# ユーザのデフォルトシェルを変更
+chsh -s /usr/local/bin/zsh
+
+
+# ==========================================================
+# 
+# homebrewで色々入れる
+# 
+# ==========================================================
+
 brew install vim
 brew install rbenv
 brew install ruby-build
