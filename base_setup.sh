5d9d23947c2e4b5bbc76f1c3bbb406d05eaac498
sekka <sekka3@gmail.com>
2015-08-17 12:57:05 +0900

    20150817

---
+++ base_setup.sh
@@ -0,0 +1,46 @@
+echo "# =========================================================="
+echo "#"
+echo "# Homebrewを導入する"
+echo "#"
+echo "# =========================================================="
+
+xcode-select --install
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap homebrew/versions
+brew doctor
+
+
+echo "# =========================================================="
+echo "#"
+echo "# zshを導入する"
+echo "#"
+echo "# =========================================================="
+
+brew install zsh
+
+# デフォルトのShellをzshにする
+# /etc/shells の末尾に /usr/local/bin/zsh を追記
+sudo sh -c 'echo $(which zsh) >> /etc/shells'
+
+# ユーザのデフォルトシェルを変更
+chsh -s /usr/local/bin/zsh
+
+
+echo "# =========================================================="
+echo "#"
+echo "# dotdilesにシンボリックリンクを貼る"
+echo "#"
+echo "# =========================================================="
+
+DOT_FILES=(.gitignore .gitignore_global .tigrc .tmux.conf .zshrc .vimrc)
+
+for file in ${DOT_FILES[@]}
+
+do
+	if [ -a $HOME/$file ]; then
+			echo "ファイルが存在するのでシンボリックリンクを貼りませんでした: $file"
+	else
+		ln -s $HOME/dotfiles/$file $HOME/$file
+			echo "シンボリックリンクを貼りました: $file"
+	fi
+done
--- setup.sh
+++ setup.sh
@@ -1,31 +1,5 @@
 echo "# =========================================================="
 echo "#"
-echo "# Homebrewを導入する"
-echo "#"
-echo "# =========================================================="
-
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap homebrew/versions
-
-
-echo "# =========================================================="
-echo "#"
-echo "# zshを導入する"
-echo "#"
-echo "# =========================================================="
-
-brew install zsh
-
-# デフォルトのShellをzshにする
-# /etc/shells の末尾に /usr/local/bin/zsh を追記
-sudo sh -c 'echo $(which zsh) >> /etc/shells'
-
-# ユーザのデフォルトシェルを変更
-chsh -s /usr/local/bin/zsh
-
-
-echo "# =========================================================="
-echo "#"
 echo "# Homebrewで色々入れる"
 echo "#"
 echo "# =========================================================="
@@ -132,26 +106,6 @@ echo "# =========================================================="
 
 brew tap peco/peco
 brew install peco
-
-
-echo "# =========================================================="
-echo "#"
-echo "# dotdilesにシンボリックリンクを貼る"
-echo "#"
-echo "# =========================================================="
-
-DOT_FILES=(.gitignore .gitignore_global .tigrc .tmux.conf .zshrc .vimrc)
-
-for file in ${DOT_FILES[@]}
-
-do
-	if [ -a $HOME/$file ]; then
-			echo "ファイルが存在するのでシンボリックリンクを貼りませんでした: $file"
-	else
-		ln -s $HOME/dotfiles/$file $HOME/$file
-			echo "シンボリックリンクを貼りました: $file"
-	fi
-done
 
 
 echo "# =========================================================="
