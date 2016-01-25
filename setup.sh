b0885cbdd96b1fcc66fe578e23f597a89df70a0f
sekka <sekka3@gmail.com>
2016-01-25 11:56:06 +0900

    20160125

--- setup.sh
+++ setup.sh
@@ -109,6 +109,7 @@ brew cask install reggy
 brew cask install rightfont
 brew cask install sequel-pro
 brew cask install shupapan
+brew cask install sketch
 brew cask install skype
 brew cask install slack
 brew cask install sourcetree
