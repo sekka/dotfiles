3421935f953c52bb8ee62223e3318d1a2c6bcc9c
sekka <sekka3@gmail.com>
2016-08-11 14:26:09 +0900

    :+1: add mas

--- setup.sh
+++ setup.sh
@@ -144,6 +144,29 @@ brew cask install xquartz
 
 ##echo "\033[0;34m==Alfredの検索対象にいれる==\033[0;39m"
 ##brew cask alfred link
+mas install 906164672 ## Form
+mas install 449589707 ## Dash
+mas install 539883307 ## LINE
+mas install 414855915 ## WinArchiver Lite
+mas install 587512244 ## Kaleidoscope
+mas install 409183694 ## Keynote
+mas install 402456742 ## Ember
+mas install 775737590 ## iA Writer
+mas install 950145466 ## Letterspace
+mas install 924891282 ## Pixave
+mas install 443987910 ## 1Password
+mas install 405580712 ## StuffIt Expander
+mas install 434290957 ## Motion
+mas install 557168941 ## Tweetbot
+mas install 497799835 ## Xcode
+mas install 880001334 ## Reeder
+mas install 498944723 ## JPEGmini
+mas install 409201541 ## Pages
+mas install 409203825 ## Numbers
+mas install 824171161 ## Affinity Designer
+mas install 568494494 ## Pocket
+mas install 944654199 ## Tree 2
+
 
 echo "\033[0;34m==メンテナンス==\033[0;39m"
 brew cask cleanup
