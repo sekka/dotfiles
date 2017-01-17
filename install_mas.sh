2be44727358461fcd45e855ac360db437733e904
sekka <sekka3@gmail.com>
2017-01-17 10:30:12 +0900

    :+1: update tasks

--- install_mas.sh
+++ install_mas.sh
@@ -9,13 +9,13 @@ mas install 539883307 ## LINE
 mas install 414855915 ## WinArchiver Lite
 mas install 587512244 ## Kaleidoscope
 mas install 409183694 ## Keynote
-mas install 402456742 ## Ember
+##mas install 402456742 ## Ember
 mas install 775737590 ## iA Writer
 mas install 950145466 ## Letterspace
 mas install 924891282 ## Pixave
 mas install 443987910 ## 1Password
 mas install 405580712 ## StuffIt Expander
-mas install 434290957 ## Motion
+##mas install 434290957 ## Motion
 mas install 557168941 ## Tweetbot
 mas install 497799835 ## Xcode
 mas install 880001334 ## Reeder
@@ -25,4 +25,6 @@ mas install 409203825 ## Numbers
 mas install 1024640650 ## CotEditor
 mas install 824171161 ## Affinity Designer
 mas install 568494494 ## Pocket
-mas install 944654199 ## Tree 2
+##mas install 944654199 ## Tree 2
+ln -s /Applications/CotEditor.app/Contents/SharedSupport/bin/cot /usr/local/bin/cot
+
