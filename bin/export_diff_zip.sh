c2d48912cf07265f13935421d81df9c96ac0a658
sekka <sekka3@gmail.com>
2017-11-07 12:32:56 +0900

    :+1: update

---
+++ bin/export_diff_zip.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+if [ "$2" = "" ]; then
+    git archive --format=zip --prefix=archive/ HEAD `git diff --name-only HEAD $1` -o archive.zip
+else
+    git archive --format=zip --prefix=archive/ $1 `git diff --name-only $1 $2` -o archive.zip
+fi
