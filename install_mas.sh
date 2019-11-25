1d3b1eccaf6e919bb600b45c893c03270d956657
sekka <sekka3@gmail.com>
2019-11-25 17:43:28 +0900

    :+1: refactor install_mas

--- install_mas.sh
+++ install_mas.sh
@@ -11,15 +11,15 @@ mas install 409183694 ## Keynote
 mas install 409201541 ## Pages
 mas install 409203825 ## Numbers
 #mas install 414855915 ## WinArchiver Lite
-mas install 434290957 ## Motion
+#mas install 434290957 ## Motion
 mas install 443987910 ## 1Password
-mas install 485812721 ## TweetDeck
+#mas install 485812721 ## TweetDeck
 mas install 497799835 ## Xcode
 mas install 498944723 ## JPEGmini
 mas install 539883307 ## LINE
 #mas install 557168941 ## Tweetbot
 mas install 587512244 ## Kaleidoscope
-mas install 775737590 ## iA Writer
+#mas install 775737590 ## iA Writer
 mas install 824171161 ## Affinity Designer
 #mas install 880001334 ## Reeder
 mas install 1024640650 ## CotEditor
