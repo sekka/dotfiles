c6263885baeca121a51b2f2bc96a923e2a594d60
sekka <sekka3@gmail.com>
2022-05-06 16:36:55 +0900

    maintenance

--- config/zsh/30_gitforesta.zsh
+++
@@ -1,4 +0,0 @@
-function gifo() { git-foresta --style=10 "$@" | less -RSX }
-function gifa() { git-foresta --all --style=10 "$@" | less -RSX }
-compdef _git gifo=git-log
-compdef _git gifa=git-log
--- setup/23_web.sh
+++ setup/23_web.sh
@@ -1,2 +1,1 @@
-curl -L https://github.com/takaaki-kasai/git-foresta/raw/master/git-foresta -o /usr/local/bin/git-foresta && chmod 755 /usr/local/bin/git-foresta
 curl -L firebase.tools | sh                                                 # Firebase
