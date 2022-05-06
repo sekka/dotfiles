1c7f49fbb16ac3442517893c9dba4251d0d5ffd8
sekka <sekka3@gmail.com>
2022-05-06 17:55:34 +0900

    add function

--- config/zsh/20_etc.zsh
+++ config/zsh/20_etc.zsh
@@ -1,2 +1,6 @@
 # cdしたあとで、自動的に pwd と lsa する
 function chpwd() { pwd; lsa }
+
+# echo $PATHの結果を整形
+# https://takuya-1st.hatenablog.jp/entry/2017/02/14/191855
+function path_show() { echo -e ${PATH//:/'\n'} }
