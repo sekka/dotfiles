dd9dd138799ae09ac9c9f40bde8ce6139567e223
sekka <sekka3@gmail.com>
2025-06-28 00:18:26 +0900

    Optimize chpwd function for automatic pwd and conditional lsa

--- config/zsh/20_etc.zsh
+++ config/zsh/20_etc.zsh
@@ -1,5 +1,15 @@
-# cdしたあとで、自動的に pwd と lsa する
-function chpwd() { pwd; lsa }
+# cdしたあとで、自動的に pwd と条件付きでlsa実行（最適化版）
+function chpwd() { 
+    pwd
+    # ファイル数が50未満の場合のみlsaを実行（パフォーマンス最適化）
+    local file_count=$(ls -1A 2>/dev/null | wc -l)
+    if [[ $file_count -lt 50 ]]; then
+        lsa
+    else
+        # 大きなディレクトリでは基本的なlsのみ
+        ls
+    fi
+}
 
 # echo $PATHの結果を整形
 # https://takuya-1st.hatenablog.jp/entry/2017/02/14/191855
