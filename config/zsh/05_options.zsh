2d351c308f74e3ccdb35a8564d1151692d244967
sekka <sekka3@gmail.com>
2025-06-28 10:24:23 +0900

    Update zsh options and enhance completion feature

--- config/zsh/05_options.zsh
+++ config/zsh/05_options.zsh
@@ -1,55 +1,20 @@
-# 日本語ファイル名を表示可能にする
-setopt print_eight_bit
-
-# beep を無効にする
-setopt no_beep
-
-# 入力しているコマンド名が間違っている場合にもしかして：を出す
-setopt correct
-
-# 色を使う
-setopt prompt_subst
-
-# ^Dでログアウトしない
-setopt ignore_eof
-
-# ^Q/^Sのフローコントロールを無効にする
-setopt no_flow_control
-
-# バックグラウンドジョブが終了したらすぐに知らせる
-setopt no_tify
-
-# '#' 以降をコメントとして扱う
-setopt interactive_comments
-
-# ディレクトリ名だけでcdする
-setopt auto_cd
-
-# cd したら自動的にpushdする
-setopt auto_pushd
-
-# 重複したディレクトリを追加しない
-setopt pushd_ignore_dups
-
-# 補完候補リストを詰めて表示
-setopt list_packed
-
-# 補完候補にファイルの種類も表示する
-setopt list_types
-
-# カッコの対応などを自動的に補完する
-setopt auto_param_keys
-
-# = の後はパス名として補完する
-setopt magic_equal_subst
-
-# 補完候補が複数あるときに自動的に一覧表示する
-setopt auto_menu
-
-# 高機能なワイルドカード展開を使用する
-setopt extended_glob
-
-# --- 高度なzshオプション ---
+setopt print_eight_bit        # 日本語ファイル名を表示可能にする
+setopt no_beep                # beep を無効にする
+setopt correct                # 入力しているコマンド名が間違っている場合にもしかして：を出す
+setopt prompt_subst           # 色を使う
+setopt ignore_eof             # ^Dでログアウトしない
+setopt no_flow_control        # ^Q/^Sのフローコントロールを無効にする
+setopt no_tify                # バックグラウンドジョブが終了したらすぐに知らせる
+setopt interactive_comments   # '#' 以降をコメントとして扱う
+setopt auto_cd                # ディレクトリ名だけでcdする
+setopt auto_pushd             # cd したら自動的にpushdする
+setopt pushd_ignore_dups      # 重複したディレクトリを追加しない
+setopt list_packed            # 補完候補リストを詰めて表示
+setopt list_types             # 補完候補にファイルの種類も表示する
+setopt auto_param_keys        # カッコの対応などを自動的に補完する
+setopt magic_equal_subst      # = の後はパス名として補完する
+setopt auto_menu              # 補完候補が複数あるときに自動的に一覧表示する
+setopt extended_glob          # 高機能なワイルドカード展開を使用する
 
 # 補完機能の強化
 setopt auto_list              # 曖昧な補完で自動的に選択肢を一覧表示
