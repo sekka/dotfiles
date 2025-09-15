921c7998006c7bed727f3f4d4bc1f77fd76176b2
sekka <sekka3@gmail.com>
2025-09-15 21:17:59 +0900

    chore: Pythonセットアップをuvに変更
    pyenvとHomebrewベースのセットアップから、uvを使用した
    モダンなPython環境管理に変更

--- setup/12_python.sh
+++ setup/12_python.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 # Python and pyenv setup script
 
-brew update
-brew upgrade
-
-anyenv install pyenv
-#exec $SHELL -l
-pyenv install -l
-
-#pyenv install 3.7.4
-pyenv install 2.7.18
-#pyenv global 3.7.4
-pyenv global 2.7.18
-
-pyenv rehash
-pyenv versions
-python -V
-which python
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv self update
