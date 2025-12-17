73e68ee80195ee533becdb78c7acebe1dd163449
sekka <sekka3@gmail.com>
2025-12-18 08:20:36 +0900

    feat: gh拡張機能（gh-dash, gh-branch）を追加
    PRダッシュボード表示やブランチ管理を効率化するため、
    gh-dashとgh-branchを追加。既存のgh-syncにもコメントで
    参照URLを追記。

--- setup/23_web.sh
+++ setup/23_web.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Web development tools setup script
 
-# https://zenn.dev/him0/articles/b5e555d98e79ee
-gh extension install him0/gh-sync
+gh extension install him0/gh-sync       # https://zenn.dev/him0/articles/b5e555d98e79ee
+gh extension install dlvhdr/gh-dash     # https://zenn.dev/yuta28/articles/gh-dash-introduction
+gh extension install mislav/gh-branch   # https://github.com/mislav/gh-branch
