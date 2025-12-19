70c6b735b0033b3b041e9b727ccc90497319aa47
sekka <sekka3@gmail.com>
2025-12-19 22:15:21 +0900

    chore: セットアップスクリプトのshfmt自動フォーマット
    shfmtによる変数クォート処理の最適化。
    コードの動作は変わらず、フォーマットのみの変更。

--- setup/11_web.sh
+++ setup/11_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Web development tools setup script
 
-gh extension install him0/gh-sync       # https://zenn.dev/him0/articles/b5e555d98e79ee
-gh extension install dlvhdr/gh-dash     # https://zenn.dev/yuta28/articles/gh-dash-introduction
-gh extension install mislav/gh-branch   # https://github.com/mislav/gh-branch
+gh extension install him0/gh-sync     # https://zenn.dev/him0/articles/b5e555d98e79ee
+gh extension install dlvhdr/gh-dash   # https://zenn.dev/yuta28/articles/gh-dash-introduction
+gh extension install mislav/gh-branch # https://github.com/mislav/gh-branch
