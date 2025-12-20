c8d13e737300ec3e6c9d347d513577e572ef61d8
sekka <sekka3@gmail.com>
2025-12-20 15:06:08 +0900

    style: shfmtによるシェルスクリプトの自動整形
    shellcheckとshfmt導入に伴う自動フォーマット適用

--- setup/11_web.sh
+++ setup/11_web.sh
@@ -5,14 +5,14 @@ gh extension install him0/gh-sync     # https://zenn.dev/him0/articles/b5e555d98
 gh extension install dlvhdr/gh-dash   # https://zenn.dev/yuta28/articles/gh-dash-introduction
 gh extension install mislav/gh-branch # https://github.com/mislav/gh-branch
 
-gibo update                           # .gitignoreテンプレートを最新に更新
+gibo update # .gitignoreテンプレートを最新に更新
 
 # CotEditorのシンボリックリンクを作成
 COT_SOURCE="/Applications/CotEditor.app/Contents/SharedSupport/bin/cot"
 COT_TARGET="/usr/local/bin/cot"
 
-if [[ -f "$COT_SOURCE" ]]; then
-  if [[ ! -L "$COT_TARGET" ]]; then
+if [[ -f $COT_SOURCE ]]; then
+  if [[ ! -L $COT_TARGET ]]; then
     echo "Creating Cot symlink..."
     sudo ln -s "$COT_SOURCE" "$COT_TARGET"
     echo "Cot symlink created."
