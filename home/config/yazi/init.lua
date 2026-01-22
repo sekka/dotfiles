7d6697548caff7f870e4e66dbdaeda8c6356bf7c
sekka <sekka3@gmail.com>
2026-01-22 10:56:28 +0900

    ui: yazi UI改善とカスタムlinemode追加
    - init.lua：
      - カスタムlinemode \"size_and_mtime\" を追加
      - ファイルサイズと更新日時を同時表示
    
    - yazi.toml：
      - [manager] を [mgr] に変更（新しい設定キー）
      - linemode を \"size\" から \"size_and_mtime\" に変更
    
    - theme.toml：
      - [manager] を [mgr] に変更
      - mgr セクションにボーダー設定を追加（細線、暗めグレー）
      - 各ダイアログ (pick, input, cmp, tasks, spot) にボーダー色を設定
      - Catppuccin color scheme (surface0) を適用

--- home/config/yazi/init.lua
+++ home/config/yazi/init.lua
@@ -29,3 +29,16 @@ function Status:githead()
 
   return ui.Line {}
 end
+
+-- カスタムlinemode: ファイルサイズと更新日時を同時に表示
+function Linemode:size_and_mtime()
+  local time = math.floor(self._file.cha.mtime or 0)
+  if time == 0 then
+    time = ""
+  else
+    time = os.date("%Y-%m-%d %H:%M", time)
+  end
+
+  local size = self._file:size()
+  return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end
