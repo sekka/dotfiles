bb279564d155f71112444442e9626de669ec0617
sekka <sekka3@gmail.com>
2026-02-11 15:59:12 +0900

    feat: Yazi tmux popup 機能を実装
    yazi ファイラーを tmux popup で起動し、選択したディレクトリに自動的に
    移動する機能を追加。Prefix + Ctrl-f でファイラーをpopup表示できる。
    
    - scripts/system/yazi-popup.sh: yazi 起動・ディレクトリ変更スクリプト
    - home/.tmux.conf: popup キーバインディング（C-f）を追加

---
+++ scripts/system/yazi-popup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+pane_id="$1"
+[ -z "$pane_id" ] && exit 1
+
+tmp=$(mktemp -t yazi-cwd.XXXXXX)
+yazi --cwd-file="$tmp"
+
+if cwd=$(cat -- "$tmp") && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+  tmux send-keys -t "$pane_id" " cd \"$cwd\"" Enter
+fi
+
+rm -f -- "$tmp"
